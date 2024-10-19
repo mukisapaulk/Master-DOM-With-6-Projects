aa23d5ff8875a5ca2b4152ace5ca54b82a361b07
mukisapaulk <mukisapaul480@gmail.com>
2024-10-19 20:35:13 +0300

    almost about to finish

--- Starter/8. createElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
+++ Starter/8. createElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
@@ -16,3 +16,22 @@
 // removeChild()
 // remove()
 // ------------------------------------
+
+//creating elements
+
+// const h1 = document.createElement('h1');
+// h1.textContent = 'helloworld';
+// h1.classList.add('greeting');
+// // console.log(h1);
+
+// //apending the create element to the html body
+// const body = document.body;
+// body.appendChild(h1)
+
+const ul = document.querySelector('ul');
+const newli = document.querySelector('li');
+newli.innerText = 'lam a new li';
+ul.appendChild(newli);
+
+const firstli = document.querySelector('li');
+ul.insertBefore(newli,firstli);
